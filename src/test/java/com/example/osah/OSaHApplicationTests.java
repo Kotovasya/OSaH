0000c8cd00525400a798b212fa4ecbacf978d6c1
Виктор Попов <loew.vity@gmail.com>
2022-09-07 22:46:31 +0300

    Start

---
+++ src/test/java/com/example/osah/OSaHApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.osah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OSaHApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
